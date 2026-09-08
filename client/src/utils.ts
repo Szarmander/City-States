c2243edb19c71e3ba4e2bd8f89cc90618bdc9ecd
Jakub Szrama <kubuszrama@gmail.com>
2026-09-08 21:57:36 +0200

    Fix: Massively scale down and recenter accessory overlays

--- client/src/utils.ts
+++ client/src/utils.ts
@@ -1,9 +1,9 @@
 import React from 'react';
 
 export const ACCESSORY_STYLES: Record<string, React.CSSProperties> = {
-  'bow_tie.png': { width: '50%', top: '75%', left: '25%' },
-  'crown.png': { width: '60%', top: '-10%', left: '20%' },
-  'funny_glasses.png': { width: '70%', top: '25%', left: '15%' },
-  'hat.png': { width: '65%', top: '-15%', left: '17.5%' },
-  'mustache.png': { width: '50%', top: '50%', left: '25%' }
+  'bow_tie.png': { width: '25%', top: '75%', left: '37.5%' },
+  'crown.png': { width: '35%', top: '-5%', left: '32.5%' },
+  'funny_glasses.png': { width: '40%', top: '35%', left: '30%' },
+  'hat.png': { width: '40%', top: '-10%', left: '30%' },
+  'mustache.png': { width: '30%', top: '60%', left: '35%' }
 };
