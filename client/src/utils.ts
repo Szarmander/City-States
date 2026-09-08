26af159027039b703e308b151a4c0c4a9c0a0e3e
Jakub Szrama <kubuszrama@gmail.com>
2026-09-08 22:10:14 +0200

    Fix: moving mustache higher

--- client/src/utils.ts
+++ client/src/utils.ts
@@ -6,5 +6,5 @@ export const ACCESSORY_STYLES: Record<string, React.CSSProperties> = {
   'bow_tie.png': { ...baseStyle, width: '35%', top: '75%', left: '32.5%' },
   'crown.png': { ...baseStyle, width: '50%', top: '-15%', left: '25%' },
   'hat.png': { ...baseStyle, width: '55%', top: '-25%', left: '22.5%' },
-  'mustache.png': { ...baseStyle, width: '45%', top: '45%', left: '27.5%' }
+  'mustache.png': { ...baseStyle, width: '45%', top: '30%', left: '27.5%' }
 };
